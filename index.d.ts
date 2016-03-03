551197bca620fc66d65744d9a6aaeed71965fc73
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2016-03-03 19:22:41 -0300

    dbmonster

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 declare class DBMonster extends mz.widgets.BasePagelet {
     dbs: any;
-    db_data: any;
-    db_data_changed(col: any): void;
+    setData(col: any): void;
     constructor();
 }
--- index.ts
+++ index.ts
@@ -5,45 +5,45 @@
             <td class="dbname">{scope.name}</td>
 
             <td class="query-count">
-                <span class="{ scope.countClassName }">{ scope.queries.length }</span>
+                <span class="{scope.countClassName}">{scope.queries.length}</span>
             </td>
 
-            <td class="Query { scope.topFiveQueries[0].className }">
-                { scope.topFiveQueries[0].elapsed }
+            <td class="Query {scope.topFiveQueries[0].className}">
+                {scope.topFiveQueries[0].elapsed }
                 <div class="popover left">
-                    <div class="popover-content">{ scope.topFiveQueries[0].query }</div>
+                    <div class="popover-content">{scope.topFiveQueries[0].query}</div>
                     <div class="arrow"></div>
                 </div>
             </td>
             
-            <td class="Query { scope.topFiveQueries[1].className }">
-                { scope.topFiveQueries[1].elapsed }
+            <td class="Query {scope.topFiveQueries[1].className}">
+                {scope.topFiveQueries[1].elapsed }
                 <div class="popover left">
-                    <div class="popover-content">{ scope.topFiveQueries[1].query }</div>
+                    <div class="popover-content">{scope.topFiveQueries[1].query}</div>
                     <div class="arrow"></div>
                 </div>
             </td>
             
-            <td class="Query { scope.topFiveQueries[2].className }">
-                { scope.topFiveQueries[2].elapsed }
+            <td class="Query {scope.topFiveQueries[2].className}">
+                {scope.topFiveQueries[2].elapsed }
                 <div class="popover left">
-                    <div class="popover-content">{ scope.topFiveQueries[2].query }</div>
+                    <div class="popover-content">{scope.topFiveQueries[2].query}</div>
                     <div class="arrow"></div>
                 </div>
             </td>
             
-            <td class="Query { scope.topFiveQueries[3].className }">
-                { scope.topFiveQueries[3].elapsed }
+            <td class="Query {scope.topFiveQueries[3].className}">
+                {scope.topFiveQueries[3].elapsed}
                 <div class="popover left">
-                    <div class="popover-content">{ scope.topFiveQueries[3].query }</div>
+                    <div class="popover-content">{scope.topFiveQueries[3].query}</div>
                     <div class="arrow"></div>
                 </div>
             </td>
             
-            <td class="Query { scope.topFiveQueries[4].className }">
-                { scope.topFiveQueries[4].elapsed }
+            <td class="Query {scope.topFiveQueries[4].className}">
+                {scope.topFiveQueries[4].elapsed}
                 <div class="popover left">
-                    <div class="popover-content">{ scope.topFiveQueries[4].query }</div>
+                    <div class="popover-content">{scope.topFiveQueries[4].query}</div>
                     <div class="arrow"></div>
                 </div>
             </td>
@@ -56,10 +56,7 @@ class DBMonster extends mz.widgets.BasePagelet {
     @DBMonster.proxy
     dbs;
     
-    @DBMonster.proxy
-    db_data: any;
-    
-    db_data_changed(col){
+    setData(col){
         this.dbs.mergeArray(col);
     }
     
